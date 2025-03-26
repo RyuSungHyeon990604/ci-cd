4dbc21ff2bec5e7cd408e27da745423bd670c9c8
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-03-26 22:48:02 +0900

    v4 리뷰봇 테스트22334455

--- src/main/java/com/example/cicdtest/controller/MyController.java
+++ src/main/java/com/example/cicdtest/controller/MyController.java
@@ -8,24 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @GetMapping("/")
-    public int home(){
+    public int sub(){
 
         int a = 1;
         int b = 0;
-        int c = a / b; // 0으로 나누기 위험
-        int d = a / b; // 0으로 나누기 위험
-        int e = a / b; // 0으로 나누기 위험
-        int f = a / b; // 0으로 나누기 위험
-        int h = a / b; // 0으로 나누기 위험
-        int k = a / b; // 0으로 나누기 위험
-        int kl = a / b; // 0으로 나누기 위험
-        int kl2 = a / b; // 0으로 나누기 위험
-        int kl3 = a / b; // 0으로 나누기 위험
-        int kl4 = a / b; // 0으로 나누기 위험
-        int kl5 = a / b; // 0으로 나누기 위험
-        int kl6 = a / b; // 0으로 나누기 위험
-        int kl7 = a / b; // 0으로 나누기 위험
-
-        return kl7;
+        return a+b;
     }
 }
