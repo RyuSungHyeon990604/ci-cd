8d99d924d031e424b675ed68022b011622832271
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-03-26 19:59:56 +0900

    v4 리뷰봇 테스트

--- src/main/java/com/example/cicdtest/CiCdTestApplication.java
+++ src/main/java/com/example/cicdtest/CiCdTestApplication.java
@@ -9,6 +9,7 @@ public class CiCdTestApplication {
     public static void main(String[] args) {
         //이렇게 해보자
         System.out.println("LGTM 말고 다른거 응답해줘봐");
+        System.out.println("프롬프트 입력했다 딱대");
 
     }
 
